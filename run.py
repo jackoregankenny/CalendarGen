19fdf3c0b2606fb95fc20d3f004105c97da686e7
jackoregankenny <42699197+jackoregankenny@users.noreply.github.com>
2024-10-29 23:49:26 +0000

    Refactor run.py to update project root path and import statement

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 import os
 import sys
 
-# Add the project root directory to Python path
+# Get the absolute path of the project root
 project_root = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, project_root)
+src_path = os.path.join(project_root, 'src')
+sys.path.insert(0, src_path)
 
-from webapp.app import app
+from calendargen.webapp.app import app
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 4321))  # Default to 4321
+    port = int(os.environ.get('PORT', 4321))
     app.run(host='0.0.0.0', port=port, debug=True)
